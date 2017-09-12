ffd75360d189734798970c4f2f81b9195493b074
apw <anggitprayogo@gmail.com>
2017-09-12 14:14:59 +0700

    revisi MainFeedDrawer

--- app/src/main/java/com/mycampus/rontikeky/myacademic/Activity/MainFeedDrawer.java
+++ app/src/main/java/com/mycampus/rontikeky/myacademic/Activity/MainFeedDrawer.java
@@ -1,8 +1,13 @@
 package com.mycampus.rontikeky.myacademic.Activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,11 +17,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.mycampus.rontikeky.myacademic.Fragment.FtiFragment;
+import com.mycampus.rontikeky.myacademic.Fragment.HomeFragment;
+import com.mycampus.rontikeky.myacademic.Fragment.SeminarFragment;
+import com.mycampus.rontikeky.myacademic.Fragment.WorkshopFragment;
 import com.mycampus.rontikeky.myacademic.R;
 
 public class MainFeedDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    SharedPreferences sharedPreferences;
+    String token_key = "token";
+    String mypref = "MYPREFRENCES";
+    String faculty_key = "faculty";
+    String fromDate_key = "fromDate";
+    String toDate_key = "toDate";
+    String token;
+    String faculty;
+    String fromDateS;
+    String toDateS;
+
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +48,8 @@ public class MainFeedDrawer extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setTitle("BLu Feed");
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +59,10 @@ public class MainFeedDrawer extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_container, new HomeFragment()).commit();
     }
 
     @Override
@@ -54,12 +75,12 @@ public class MainFeedDrawer extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_feed_drawer, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main_feed_drawer, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -76,6 +97,8 @@ public class MainFeedDrawer extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -83,32 +106,70 @@ public class MainFeedDrawer extends AppCompatActivity
         NavigationView nv= (NavigationView) findViewById(R.id.nav_view);
         Menu m=nv.getMenu();
         int id = item.getItemId();
-        if (id == R.id.nav_posts) {
-            boolean b=!m.findItem(R.id.nav_class).isVisible();
-            m.findItem(R.id.nav_class).setVisible(b);
-            m.findItem(R.id.nav_dept).setVisible(b);
-            m.findItem(R.id.nav_batch).setVisible(b);
-            m.findItem(R.id.nav_campus).setVisible(b);
-            return true;
-        } else if (id == R.id.nav_walls) {
-            boolean b=!m.findItem(R.id.nav_wall_events).isVisible();
-            m.findItem(R.id.nav_wall_events).setVisible(b);
-            m.findItem(R.id.nav_wall_fun).setVisible(b);
-            m.findItem(R.id.nav_wall_hadith).setVisible(b);
-            m.findItem(R.id.nav_wall_news).setVisible(b);
-            m.findItem(R.id.nav_wall_Poetry).setVisible(b);
-            return true;
-        } else if (id == R.id.nav_com) {
-            m.findItem(R.id.nav_share).setVisible(false);
-            m.findItem(R.id.nav_send).setVisible(false);
 
-        } else if (id == R.id.nav_share) {
+        fragment = null;
 
-        } else if (id == R.id.nav_send) {
+        if (id == R.id.nav_info) {
+            boolean b=!m.findItem(R.id.nav_baak).isVisible();
+            m.findItem(R.id.nav_baak).setVisible(b);
+            m.findItem(R.id.nav_fti).setVisible(b);
+            m.findItem(R.id.nav_ft).setVisible(b);
+            m.findItem(R.id.nav_fe).setVisible(b);
+            m.findItem(R.id.nav_fikom).setVisible(b);
+            return true;
+        } else if (id == R.id.nav_seminar) {
+            fragment = new SeminarFragment();
+        } else if (id == R.id.nav_workshop) {
+            fragment = new WorkshopFragment();
+        } else if (id == R.id.nav_baak) {
+            prefEditor("BAAK");
+        } else if (id == R.id.nav_fti) {
+            prefEditor("FTI");
+        }else if (id == R.id.nav_ft) {
+            prefEditor("FT");
+        }else if (id == R.id.nav_fe) {
+            prefEditor("FE");
+        }else if (id == R.id.nav_fikom) {
+            prefEditor("FIKOM");
+        }else if (id == R.id.nav_about) {
+            Intent intent = new Intent(MainFeedDrawer.this, AboutActivity.class);
+            startActivity(intent);
+        }else if (id == R.id.nav_logout) {
+
+            SharedPreferences preferences = getSharedPreferences(token_key, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.clear();
+            editor.commit();
+
+            Intent i=new Intent(MainFeedDrawer.this,splash_screen.class);
+            i.setAction(Intent.ACTION_MAIN);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            finish();
+            try {startActivity(i);}
+            catch (java.lang.Exception ignore) {}
+            System.exit(0);
+
+            Toast.makeText(getApplicationContext(),"Anda Berhasil Logout",Toast.LENGTH_LONG).show();
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frame_container, fragment).commit();
 
         }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void prefEditor(String faculty){
+        sharedPreferences = getSharedPreferences(token_key, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(faculty_key, faculty);
+        editor.commit();
+        fragment = new HomeFragment();
     }
 }
